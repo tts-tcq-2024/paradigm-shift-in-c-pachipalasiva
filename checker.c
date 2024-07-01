9d536d6279982e676548a87c23298f8e39e34687
pachipalasiva <120707070+pachipalasiva@users.noreply.github.com>
2024-07-01 14:51:24 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,20 +1,20 @@
 #include <stdbool.h>
 #include <assert.h>
 
-bool is_temperature_out_of_range(float temperature) {
+bool istemperatureoutofrange(float temperature) {
     return temperature < 0 || temperature > 45;
 }
 
-bool is_soc_out_of_range(float soc) {
+bool issocoutofrange(float soc) {
     return soc < 20 || soc > 80;
 }
 
-bool is_charge_rate_out_of_range(float charge_rate) {
+bool ischargerateoutofrange(float charge_rate) {
     return charge_rate > 0.8;
 }
 
 bool batteryIsOk(float temperature, float soc, float charge_rate) {
-    return !(is_temperature_out_of_range(temperature) || is_soc_out_of_range(soc) || is_charge_rate_out_of_range(charge_rate));
+    return !(istemperatureoutofrange(temperature) || issocoutofrange(soc) || ischargerateoutofrange(charge_rate));
 }
 
 int main() {
