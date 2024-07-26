aa350945cde5a0317ae053cd475e1f354544f0d2
pachipalasiva <120707070+pachipalasiva@users.noreply.github.com>
2024-07-26 11:07:54 +0530

    battery_monitor.c

--- battery_monitor.c
+++ battery_monitor.c
@@ -29,7 +29,3 @@ int batteryIsOk(float temperature, float soc, float chargeRate) {
     return ((checktemprange(temperature))&&(checksoc(soc))&&(checkchargerate(chargeRate)));
 }
  
-int main() {
-  assert(batteryIsOk(25, 70, 0.7));
-  assert(!batteryIsOk(50, 85, 0));
-}
