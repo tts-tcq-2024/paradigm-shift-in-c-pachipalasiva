f05dd9b1061c83ca2d52c808b32b0239a83cc33f
pachipalasiva <120707070+pachipalasiva@users.noreply.github.com>
2024-08-04 17:20:45 +0530

    Update TemperatureCheck.c

--- TemperatureCheck.c
+++ TemperatureCheck.c
@@ -1,5 +1,5 @@
 #include "TemperatureCheck.h"
-int checktemprange(float temperature) {   //checking temperature in range or not
+void checktemprange(float temperature) {   //checking temperature in range or not
   if(temperature < 0 || temperature > 45) {
     printf("Temperature out of range!\n");
     return 0;
