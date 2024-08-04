a0f0eb96081023911a5cb5bc593048fd44c8fb8c
pachipalasiva <120707070+pachipalasiva@users.noreply.github.com>
2024-08-04 16:26:41 +0530

    Update alert.c

--- alert.c
+++ alert.c
@@ -1,16 +1,17 @@
 #include "alert.h"
 
 void approchingDischart(float rate) {
-    if(rate >= 20 && rate <==24)
+    if(rate >= 20 && rate <=24)
           printf("Warning: Approaching discharge");
 }
 
 
 void approchingPeak(float rate) {
     if(rate >=76 && rate <= 80) {
-        printf("Warning: Approaching charge-peak");
+        printf("Warning: Approaching charge_peak");
 }
 void showAlert(float rate) {
 approchingDischart(rate);
-    approchingPeak(rate);
+approchingPeak(rate);
+}
 }
